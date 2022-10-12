e777e6a30e3c8dc8434c944681d8454bad6bad40
justinpaulosolo <justinpaulosolo@gmail.com>
2022-10-12 00:48:03 -0700

    feat: Button component

--- src/pages/testing.tsx
+++ src/pages/testing.tsx
@@ -1,3 +1,4 @@
+import { Button } from '../components/common/button';
 import Container from '../components/home/Container';
 import { trpc } from '../utils/trpc';
 
@@ -7,11 +8,62 @@ export default function Testing() {
   const { data } = trpc.github.getReposTest.useQuery();
 
   console.log(data);
+  const loading = false;
 
   return (
     <Container>
-      <div>
-        <h1>Testing Page</h1>
+      <div className="flex flex-col gap-5">
+        <h1>Primary</h1>
+        <div className="flex gap-5">
+          <Button
+            variant="primary"
+            size="lg"
+            loading={loading}
+          >
+            Dashboard
+          </Button>
+          <Button
+            variant="primary-inverted"
+            loading={loading}
+            size="lg"
+          >
+            Dashboard
+          </Button>
+        </div>
+        <h1>Secondary</h1>
+        <div className="flex gap-5">
+          <Button
+            variant="secondary"
+            size="lg"
+            loading={loading}
+          >
+            Dashboard
+          </Button>
+          <Button
+            variant="secondary-inverted"
+            loading={loading}
+            size="lg"
+          >
+            Dashboard
+          </Button>
+        </div>
+        <h1>Danger</h1>
+        <div className="flex gap-5">
+          <Button
+            variant="danger"
+            size="lg"
+            loading={loading}
+          >
+            Dashboard
+          </Button>
+          <Button
+            variant="danger-inverted"
+            loading={loading}
+            size="lg"
+          >
+            Dashboard
+          </Button>
+        </div>
       </div>
     </Container>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          0: '#fff',
+          100: '#fafafa',
+          200: '#eaeaea',
+          300: '#999999',
+          400: '#888888',
+          500: '#666666',
+          600: '#444444',
+          700: '#333333',
+          800: '#222222',
+          900: '#111111',
+        },
+      },
+    },
   },
   plugins: [
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    require('@headlessui/tailwindcss')({ prefix: 'ui' })
+    require('@headlessui/tailwindcss')({ prefix: 'ui' }),
   ],
 };
